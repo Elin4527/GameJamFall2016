efa3397a5e29ccacc9bae24d7aff684017bea62e
WildV <elin4527@gmail.com>
2016-10-29 14:46:55 -0400

    Attack and Graze
    Added attack and graze. Decoupled chars from rigidbody

--- BulletBlade/Assets/Scripts/Character/Player.cs
+++ BulletBlade/Assets/Scripts/Character/Player.cs
@@ -9,11 +9,18 @@ public class Player : BaseCharacter {
     }
 
     ActionState state = ActionState.STANDARD;
+    public GameObject atkObj;
     public float dashDuration = 0.3f;
     public float invincibleDuration = 0.4f;
+    public float attackCd;
     float invincibleTIme;
     float actionTime;
     Animator anim;
+    public int damage;
+    float cdRemain = 0;
+    public int grazeCount = 0;
+    bool graze = false;
+    
 
 	// Use this for initialization
 	protected override void init () {
@@ -22,11 +29,18 @@ public class Player : BaseCharacter {
 
 	// Update is called once per frame
 	public override void logic () {
+        direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+
         switch (state)
         {
+
             case ActionState.DASHING:
                 {
                     actionTime -= Time.deltaTime;
+                    SpriteRenderer s = GetComponent<SpriteRenderer>();
+                    
+                    //GameObject g = (GameObject)Instantiate(GetComponent<SpriteRenderer>(), transform.position, Quaternion.identity);
+                    //Destroy(g, 0.5f);
                     if (actionTime < 0.0)
                     {
                         state = ActionState.STANDARD;
@@ -44,6 +58,11 @@ public class Player : BaseCharacter {
                 break;
         }
         invincibleTIme -= Time.deltaTime;
+        if (graze)
+        {
+            grazeCount++;
+            graze = false;
+        }
     }
 
     public bool isTangible()
@@ -62,11 +81,10 @@ public class Player : BaseCharacter {
     {
         bool walk = isWalking();
         anim.SetBool("isWalking", walk);
-        if (walk)
-        {
-            anim.SetFloat("x", rb.velocity.x);
-            anim.SetFloat("y", rb.velocity.y);
-        }
+
+            anim.SetFloat("x", 0);
+            anim.SetFloat("y", -1);
+        
     }
 
     void dash()
@@ -77,14 +95,28 @@ public class Player : BaseCharacter {
 
         // Dash towards mouse cursor
         Vector2 towards = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-        setVelocity(towards.normalized * Mathf.Min((speed * 2.0f * rb.velocity.magnitude), towards.magnitude/dashDuration));
-        anim.SetFloat("x", rb.velocity.x);
-        anim.SetFloat("y", rb.velocity.y);
+        setVelocity(towards.normalized * Mathf.Min((speed * 2.0f * vel.magnitude), towards.magnitude/dashDuration));
+        anim.SetFloat("x", vel.x);
+        anim.SetFloat("y", vel.y);
         anim.speed = 4.0f;
+    }
+
+    void attack()
+    {
+        GameObject g = (GameObject)Instantiate(atkObj, transform.position + (Vector3)direction.normalized * 1, 
+            Quaternion.Euler(0, 0, ((direction.y < 0) ? -1 : 1) * Vector2.Angle(Vector2.right, direction)));
+        Attack a = g.GetComponent<Attack>();
+        a.damage = damage;
     }
 
     public override Vector2 getInput()
     {
+        cdRemain -= Time.deltaTime;
+        if (Input.GetMouseButton(1) && cdRemain <= 0)
+        {
+            attack();
+            cdRemain = attackCd;
+        }
         if (state == ActionState.STANDARD)
         {
             float x = Input.GetAxisRaw("Horizontal");
@@ -95,10 +127,18 @@ public class Player : BaseCharacter {
             {
                 dash();
             }
-            
+
             return (new Vector2(x, y).normalized * speed);
         }
         else
-            return rb.velocity;
+            return vel;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Bullet>())
+        {
+            graze = true;
+        }
     }
 }
