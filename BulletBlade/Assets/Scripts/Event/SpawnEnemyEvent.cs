37a4eee8789aac09b5ec0f7a7099c49b8864646a
WildV <elin4527@gmail.com>
2016-10-30 03:23:57 -0400

    Merge remote-tracking branch 'refs/remotes/origin/tilemap-branch' into Edwin-Branch
    # Conflicts:
    #	BulletBlade/Assets/BulletBlade.unity

---
+++ BulletBlade/Assets/Scripts/Event/SpawnEnemyEvent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+[Serializable]
+public class SpawnEnemyEvent : TimelineEvent {
+
+    public float startTime;
+    public float endTime;
+    public Vector3 spawnPos;
+    public GameObject enemy;
+
+    private GameObject clonedEnemy;
+
+
+
+
+    public SpawnEnemyEvent(float ti, float tf, Transform parentTransform, Vector3 spawnPos, GameObject enemy) 
+        : base(ti, tf, parentTransform)
+    {
+        this.spawnPos = spawnPos;
+        this.enemy = enemy;
+    }
+
+    public override void onStart()
+    {
+        clonedEnemy = Instantiate(enemy, parentTransform) as GameObject;
+        clonedEnemy.transform.Translate(spawnPos);
+
+    }
+
+    public override void onFinished()
+    {
+        // leaveScreen(); or // despawn(); etc...
+
+    }
+
+   
+}
---
+++ BulletBlade/Assets/Scripts/Event/TimelineEvent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public abstract class TimelineEvent : MonoBehaviour {
+
+    protected float timeStart;
+    protected float timeEnd;
+    protected Transform parentTransform;
+
+    public Boolean hasStarted = false;
+    public Boolean hasFinished = false;
+
+
+    protected TimelineEvent(float ti, float tf, Transform parentTransform)
+    {
+        this.parentTransform = parentTransform;
+        setStartTime(ti);
+        setEndTime(tf);
+    }
+
+    public void setStartTime(float ti)
+    {
+        timeStart = ti;
+    }
+
+    public void setEndTime(float tf)
+    {
+        timeEnd = tf;
+    }
+
+    public float getStartTime()
+    {
+        return this.timeStart;
+    }
+
+    public float getEndTime()
+    {
+        return this.timeEnd;
+    }
+
+    public abstract void onStart();
+
+    public abstract void onFinished();
+
+
+	
+
+}
