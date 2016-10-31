785b0d0b09047ae271f94c5f7fd7953786b548c7
WildV <elin4527@gmail.com>
2016-10-31 14:50:36 -0400

    UPDATE

---
+++ BulletBlade/Assets/Scripts/Attack/AttackPattern.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AttackPattern : MonoBehaviour {
+
+    [Serializable]
+    public class AttackEvent
+    {
+        public GameObject bullet, behaviour;
+
+        public GameObject spawner;
+        public int bullets;
+        public float time;
+        public int repeat;
+        public int round;
+        public float cd;
+        public float offset;
+        public bool track;
+        public Color c = Color.white;
+    }
+    Enemy e;
+
+    public AttackEvent[] attackEventList;
+
+	// Use this for initialization
+	void Start () {
+        e = GetComponentInParent<Enemy>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float elapsedTime = Time.fixedDeltaTime;
+        for (int i = 0; i < attackEventList.Length; i++)
+        {
+            AttackEvent a = attackEventList[i];
+
+            a.time -= elapsedTime;
+            while (a.time < 0 && (a.repeat == 0 || a.round < a.repeat))
+            {
+                GameObject g = Instantiate(a.spawner, e.gameObject.transform.position, Quaternion.identity) as GameObject;
+                g.transform.SetParent(e.transform);
+                BulletSpawner s = g.GetComponent<BulletSpawner>();
+
+                s.bulletCount = a.bullets;
+                s.prefab = a.bullet;
+                s.behaviour = a.behaviour;
+                s.clr = a.c;
+                if(a.track) s.track = e.p.gameObject;
+                s.offset += a.offset;
+                a.round++;
+                a.time += a.cd;
+            }
+        }
+	}
+}
---
+++ BulletBlade/Assets/Scripts/Bullet/DelayedHoming.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedHoming : BulletBehaviour {
+
+    bool homing = true;
+    public override void startUp()
+    {
+        
+    }
+    protected override void bulletLogicTick()
+    {
+        b.setAngle(b.velocity);
+        if (timeElapsed > 1.0f && homing)
+        {
+            b.velocity = (b.p.transform.position - b.transform.position).normalized * b.velocity.magnitude;
+            homing = false;
+        }
+    }
+}
