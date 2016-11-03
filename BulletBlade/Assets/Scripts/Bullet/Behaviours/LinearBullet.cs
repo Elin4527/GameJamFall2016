f7cb5f5b1a69e58da6601a79e011a802ade3d2ec
WildV <elin4527@gmail.com>
2016-11-03 19:30:12 -0400

    Rearranged Assets

---
+++ BulletBlade/Assets/Scripts/Bullet/Behaviours/LinearBullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LinearBullet : BulletBehaviour {
+    public override void startUp()
+    {
+    }
+    protected override void bulletLogicTick()
+    {
+        b.setAngle(b.velocity);
+    }
+}
